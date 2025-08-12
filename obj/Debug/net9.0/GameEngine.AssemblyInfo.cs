4764ebb323e047bc8f62da78b13262abc05f5442
OsmanKaradas <osmankaradas099@gmail.com>
2025-08-12 16:44:56 +0300

    Fixed Physics

--- obj/Debug/net9.0/GameEngine.AssemblyInfo.cs
+++ obj/Debug/net9.0/GameEngine.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameEngine")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+671ed1962e84f96a77f965964e25b1d250fcb4de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ebe23171232ae731025f79d254917fa98cb9e24")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameEngine")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameEngine")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
